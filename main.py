94cf9dbcbaa5c27d6f04c013aed1130cb63ef08b
jacobki <jacobki@kth.se>
2020-10-03 23:43:51 +0200

    pep8 formatting

--- main.py
+++ main.py
@@ -3,97 +3,95 @@
 import cv2
 import numpy as np
 
+
 def read_brush(size):
-	img = cv2.imread("./brushes/1.jpg", cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread("./brushes/1.jpg", cv2.IMREAD_GRAYSCALE)
 
-	# img = cv2.imread("./brushes/4-removebg-preview.png")
-	dim = (size, size)
-	img = cv2.resize(img, dim, interpolation = cv2.INTER_CUBIC)
+    # img = cv2.imread("./brushes/4-removebg-preview.png")
+    dim = (size, size)
+    img = cv2.resize(img, dim, interpolation=cv2.INTER_CUBIC)
 
-	# img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	return img
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return img
 
 
 def show_painting(img):
-	# Normalize from 0-255 to 0-1 which openCV likes =)
-	img = img / 255.0
-	cv2.imshow("image", img)
-	cv2.waitKey()
+    # Normalize from 0-255 to 0-1 which openCV likes =)
+    img = img / 255.0
+    cv2.imshow("image", img)
+    cv2.waitKey()
 
 
 def paint(img, brush, pos, brightness):
-	if img.ndim != brush.ndim:
-		raise Exception("Mismatch in img and brush dimensions")
+    if img.ndim != brush.ndim:
+        raise Exception("Mismatch in img and brush dimensions")
 
-	brush = np.multiply(brush, brightness)
+    brush = np.multiply(brush, brightness)
 
-	roi = img[pos[0]:pos[0] + brush.shape[0], pos[1]:pos[1] + brush.shape[1]]
-	roi = cv2.add(roi, brush)
-	roi = np.clip(roi, 0.0, 255.0)
-	img[pos[0]:pos[0] + brush.shape[0], pos[1]:pos[1] + brush.shape[1]] = roi.astype(np.uint8)
-	print(img)
+    roi = img[pos[0]:pos[0] + brush.shape[0], pos[1]:pos[1] + brush.shape[1]]
+    roi = cv2.add(roi, brush)
+    roi = np.clip(roi, 0.0, 255.0)
+    img[pos[0]:pos[0] + brush.shape[0], pos[1]
+        :pos[1] + brush.shape[1]] = roi.astype(np.uint8)
+    print(img)
 
+    return img
 
-	return img
 
 def main():
-	# create painting
-	width = 500
-	height = 500
-	img = np.zeros([width, height])
+    # create painting
+    width = 500
+    height = 500
+    img = np.zeros([width, height])
 
-	# load brush
-	brush_size = 50
-	brush = read_brush(brush_size)
-	print(brush.shape)
+    # load brush
+    brush_size = 50
+    brush = read_brush(brush_size)
+    print(brush.shape)
 
-	# Draw brushstrokes
-	for x in range(100):
+    # Draw brushstrokes
+    for x in range(100):
 
-		# get random pos
-		pos = (
-			np.random.randint(width - brush_size, size=1)[0],
-			np.random.randint(height - brush_size, size=1)[0]
-		)
+        # get random pos
+        pos = (
+            np.random.randint(width - brush_size, size=1)[0],
+            np.random.randint(height - brush_size, size=1)[0]
+        )
 
-		# print(pos)
-		# stroke brush on painting
-		img = paint(img, brush, pos, np.random.rand(1)[0])
+        # print(pos)
+        # stroke brush on painting
+        img = paint(img, brush, pos, np.random.rand(1)[0])
 
-
-	# show painting 
-	show_painting(img)
+    # show painting
+    show_painting(img)
 
 
 class Population:
 
-	def __init__(self, population_size):
-		self.chomosomes = []
-		self.population_size = population_size
+    def __init__(self, population_size):
+        self.chomosomes = []
+        self.population_size = population_size
 
-		for i in population_size:
+        for i in population_size:
 
-			
+    def score_chromosomes(self):
+        pass
 
-	def score_chromosomes(self):
-		pass
+    def crossover(self, chromosome_1, chromosome_2):
+        # Queue marvin gaye- lets get it on
+        pass
 
-	def crossover(self, chromosome_1, chromosome_2):
-		# Queue marvin gaye- lets get it on
-		pass
+    def mutate(self, chromosome):
+        pass
 
-	def mutate(self, chromosome):
-		pass
-	
 
 class BrushStroke:
 
-	def __init__(self, scale_factor, brightness, pos):
-		self.scale_factor = scale_factor
-		self.brightness = brightness
-		self.pos = pos
+    def __init__(self, scale_factor, brightness, pos):
+        self.scale_factor = scale_factor
+        self.brightness = brightness
+        self.pos = pos
 
 
 if __name__ == '__main__':
-	main()
-
+    main()
