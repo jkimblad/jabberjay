9ce988be614fb72017dc66b0ee5894c0a8886a66
Julian Hedén <julianheden@gmail.com>
2021-10-27 21:27:09 +0200

    CHANGE to constantly use webcam as target

--- main.py
+++ main.py
@@ -62,12 +62,11 @@ def main():
     np.random.seed(500)  # Set seed for easier debugging
     width = 500
     height = 500
+
     num_brushstrokes = 4
     kill_rate = 0.5
     mutation_rate = 0.1
-    # load target image
-    target = cv2.imread("./photos/mona.jpg", cv2.IMREAD_GRAYSCALE)
-    target = cv2.resize(target, (width, height), interpolation=cv2.INTER_CUBIC)
+
     # create painting
     canvas = np.zeros([width, height])
     # load brush
@@ -77,25 +76,28 @@ def main():
     # Create and populate population
     population = Population(20, num_brushstrokes, width, height, brush_max_size)
 
-    # Evolve unto next generation
-    next_picture = False
-    # while True:
-    num_generations = 10000
     num_evolves = 3
+
     window_name = 'Image de Lena'
     cv2.namedWindow(window_name)
     cv2.resizeWindow(window_name, 500, 500)
     cv2.moveWindow(window_name, 600, 100)
-    cv2.namedWindow("target")
-    cv2.resizeWindow("target", 500, 500)
-    cv2.moveWindow("target", 100, 100)
-    if (DEBUG):
-        cv2.namedWindow("debug")
-        cv2.resizeWindow("debug", 500, 500)
-        cv2.moveWindow("debug", 1100, 100)
 
-    show_painting("target", target)
-    for i in range(num_generations):
+    cam = cv2.VideoCapture(0)
+
+    # create painting
+    canvas = np.zeros([width, height])
+
+    while True:
+        # Read image input
+        ret, frame = cam.read()
+        if not ret:
+            print("failed to grab target")
+            break
+        target = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        target = cv2.resize(target, (width, height), interpolation=cv2.INTER_CUBIC)
+
+        # run algo on target image
         for j in range(num_evolves):
             population.evolve(
                 mutation_rate,
@@ -104,29 +106,14 @@ def main():
                 brush_img,
                 target,
                 paint)
+
         # Chose top-scoring stroke_layer and add it to canvas
         for stroke in population.stroke_layers[0].brush_strokes:
             canvas = paint(canvas, brush_img, stroke)
 
-        debug_canvas = np.array([0])
-        # Draw each StrokeLayer in the population in a new window after num_generations
-        if (DEBUG):
-            debug_canvas = np.zeros([width, height])
-            for stroke_layer in population.stroke_layers:
-                for stroke in stroke_layer.brush_strokes:
-                    debug_canvas = paint(debug_canvas, brush_img, stroke)
-
-        if i % 1 == 0:
-            if(DEBUG):
-                print("0:", population.stroke_layers[0])
-                print("1:", population.stroke_layers[1])
-                print("2:", population.stroke_layers[2])
-                show_painting("debug", debug_canvas)
-
         show_painting(window_name, canvas)
 
-    # Save image
-    cv2.imwrite("./photos/painted.png", canvas)
+
 
 if __name__ == '__main__':
     main()
